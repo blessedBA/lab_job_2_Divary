6a7613c11f7a2843eefbc9cd4028f14f6996baab
No_Name <kalmak355@gmail.com>
2025-10-24 14:13:17 +0300

    update file struct

--- precision_analysis.c
+++ precision_analysis.c
@@ -18,3 +18,19 @@ double dev_exp(double *radioactivity, double *time, int M, double decay_time) {
 
     return deviation;
 }
+
+double dev_linear(double *radioactivity, double *time, int M, double decay_rate)
+{
+    assert(radioactivity != NULL);
+    assert(time != NULL);
+    assert(M > 0);
+
+    double sum_residuals2 = 0.0, s2 = 0.0;
+    for (int i = 0; i < M; i++)
+    {
+        s2 = radioactivity[i] - (1 - time[i] / decay_rate);
+        sum_residuals2 += s2 * s2;
+    }
+    double deviation2 = sqrt(sum_residuals2) / M;
+    return deviation2;
+}
