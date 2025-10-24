616fa3b9f334eb20054dc1a58eec339fc2e19e1a
No_Name <kalmak355@gmail.com>
2025-10-24 13:11:42 +0300

    123

--- gen_header.h
+++ gen_header.h
@@ -1,8 +1,20 @@
 #ifndef GEN_HEADER_H
 #define GEN_HEADER_H
 
+//experiment
+
 int experiment (double *radioactivity, double *time, double start_time,
                 double end_time, double step);
+
+
+//nonlinear and linear equation
+
+
+
+//precision_analysis:
+
+double dev_exp(double *radioactivity, double *time, int M, double decay_time);
+
 
 
 #endif // GEN_HEADER_H
