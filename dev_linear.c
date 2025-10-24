e90ca9f2783796c4ec5e4bbf15361621f38a9d59
OlgaE <102513088+OlgaE17@users.noreply.github.com>
2025-10-24 13:45:38 +0300

    Update dev_linear.c

--- dev_linear.c
+++ dev_linear.c
@@ -6,7 +6,7 @@ double dev_linear(double *radioactivity, double *time, int M, double decay_rate)
     assert(time != NULL);
     assert(M > 0);
 
-    double sum_residuals2, s2;
+    double sum_residuals2 = 0.0, s2 = 0.0;
     for (int i = 0; i < M; i++)
     {
         s2 = radioactivity[i] - (1 - time[i] / decay_rate);
