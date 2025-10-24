f985e8feac97e507e1816539f172a46ad5a7cd93
MartAlx-RT <martynovalexvit99@gmail.com>
2025-10-24 15:03:16 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,38 @@
 #include "gen_header.h"
 
+#define NUM_OF_POINTS 100
 
-int main ()
+
+int main(void)
 {
-    const int N = 100;
-    double radioactivity[N] = {};
-    double time[N] = {};
-    double start_time = 0.0;
-    double end_time = 9.9;
-    double step = 0.1;
-    double precision = 0.0001;
+    double radioactivity[NUM_OF_POINTS] = {};
+    double time[NUM_OF_POINTS] = {};
 
-    experiment(radioactivity, time, start_time, end_time, step);
-    add_noise(radioactivity, N);
+    const double start_time= 0.0;
+    const double end_time = 9.9;
+    const double step = 0.1;
+    
+    double precision = 0, betta = 0;
+    while(scanf("%lf%lf", &precision, &betta) == 2)
+    {
+        static int count = 1;
 
+        experiment(radioactivity, time, start_time, end_time, step);
+        add_noise(radioactivity, NUM_OF_POINTS);
+
+        double decay_rate = linear_equation(radioactivity, time, NUM_OF_POINTS);
+        double decay_time = nonlinear_equation(radioactivity, time, NUM_OF_POINTS, precision);
+
+        double decay_time_err = dev_exp(radioactivity, time, NUM_OF_POINTS, decay_time);
+        double decay_rate_err = dev_linear(radioactivity, time, NUM_OF_POINTS, decay_rate);
+
+        printf("test data[%d]:\n", count++);
+        printf("decay rate = %lg\n", decay_rate);
+        printf("decay time = %lg\n", decay_time);
+        printf("time_difference = %lg\n", precision_analysis(radioactivity, time, NUM_OF_POINTS));
+
+        putchar('\n');
+
+    }
     return 0;
 }
