dc1ad85cca004b8c1fdfff532d7ddb4bebd777bd
OlgaE <102513088+OlgaE17@users.noreply.github.com>
2025-10-24 13:44:38 +0300

    Create dev_linear.c
    double dev_linear(double *radioactivity, double *time, int M, double decay_rate) – передаем указатель на массив экспериментальных и массив аргументов, количество элементов массива, границы интервала и значение decay_rate. Функция возвращает среднеквадратичное отклонение для линейной модели deviation2.

---
+++ dev_linear.c
@@ -0,0 +1,17 @@
+#include "gen_header.h"
+
+double dev_linear(double *radioactivity, double *time, int M, double decay_rate)
+{
+    assert(radioactivity != NULL);
+    assert(time != NULL);
+    assert(M > 0);
+
+    double sum_residuals2, s2;
+    for (int i = 0; i < M; i++)
+    {
+        s2 = radioactivity[i] - (1 - time[i] / decay_rate);
+        sum_residuals2 += s2 * s2;
+    }
+    double deviation2 = sqrt(sum_residuals2) / M;
+    return deviation2;
+}
